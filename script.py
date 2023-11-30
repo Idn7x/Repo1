0ca17e0bd0108a5c0c937de12981daa9e30ff7fe
NACIRI Imad-Eddine <ieddinen@gmail.com>
2023-11-30 11:34:29 +0000

    new version of script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+# Get user input as a floating-point number
+user_input = float(input("Enter a number: "))
+
+# Calculate and print the absolute value
+absolute_value = abs(user_input)
+print(f"The absolute value of {user_input} is {absolute_value}")
